40461789047981007e41a79710b835d2c70ca37f
sagnikpaul28 <sagnikpaul2882@gmail.com>
2021-07-04 01:15:01 +0530

    Changed font

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ body {
   overflow-x: hidden;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
